28e2703bcba58e350b3bcc29c31bfc37081b1b23
Nagasaivardhan <nagasaikonderu@gmail.com>
2025-11-07 14:53:31 +0530

    Update allstagesjenkinsfile.groovy

--- allstagesjenkinsfile.groovy
+++ allstagesjenkinsfile.groovy
@@ -18,26 +18,3 @@ pipeline {
         }
     }
 }
-
-// @Library('com.nagasai.sharedlib') _
-
-// pipeline {
-//     agent any
-
-//     stages {
-//         stage('Build and Deploy') {
-//             steps {
-//                 script {
-//                     // Single call that runs all stages
-//                     myPipeline(
-//                         imageName: "my-java-app:latest",
-//                         containerName: "myjava", 
-//                         hostPort: 8989,
-//                         containerPort: 8080,
-//                         skipTests: true
-//                     )
-//                 }
-//             }
-//         }
-//     }
-// }
